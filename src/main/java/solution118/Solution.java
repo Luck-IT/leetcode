8f0540c9d17c15a348c08938d1c415129f56d61d
Eric Wu <wwj198905@sina.com>
2021-11-16 10:44:47 +0800

    118

--- src/main/java/solution118/Solution.java
+++ src/main/java/solution118/Solution.java
@@ -31,7 +31,7 @@ public class Solution {
                 if(j==i-1){
                     tmp.add(1);
                 }else{
-                    tmp.add(result.get(i-1).get(j-1)+result.get(i-1).get(j));
+                    tmp.add(result.get(i-2).get(j-1)+result.get(i-2).get(j));
                 }
             }
             result.add(tmp);
