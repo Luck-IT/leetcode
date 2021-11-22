11315e9e108c05a1d1cd24302901951f475b103c
Eric Wu <wwj198905@sina.com>
2021-11-22 15:20:29 +0800

    commit309

---
+++ src/main/java/solution309/Solution.java
@@ -0,0 +1,33 @@
+package solution309;
+
+/**
+* Solution
+* #description#
+* @author:Eric
+* @since 1.8
+* create 2021年11月22日
+*/
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null||prices.length ==0){
+            return 0;
+        }
+        int HAVE_NOBUY = 0;
+        int  NO_FREEZE=1;
+        int NO_NOFREEZE = 2;
+        int[][] f = new int[prices.length][3];
+  
+        f[0][HAVE_NOBUY] = -prices[0];
+        f[0][NO_FREEZE] = 0;
+        f[0][NO_NOFREEZE] =0;
+
+        for(int i=1;i<prices.length;i++){
+           f[i][HAVE_NOBUY] = Math.max(f[i-1][NO_NOFREEZE] - prices[i],f[i-1][HAVE_NOBUY]);
+           f[i][NO_FREEZE] = f[i-1][HAVE_NOBUY]+prices[i];
+           f[i][NO_NOFREEZE] = Math.max(f[i-1][NO_FREEZE], f[i-1][NO_NOFREEZE]);
+        }
+
+        return Math.max(f[prices.length-1][NO_FREEZE],f[prices.length-1][NO_NOFREEZE]);
+
+    }
+}
