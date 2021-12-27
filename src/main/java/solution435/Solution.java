1613ac77b10624d0cbb871566aad9232cf488299
Eric Wu <wwj198905@sina.com>
2021-12-27 16:16:52 +0800

    435

---
+++ src/main/java/solution435/Solution.java
@@ -0,0 +1,36 @@
+package solution435;
+
+import java.util.Arrays;
+
+/**
+ * Solution
+ * #description#
+ * 
+ * @author:Eric
+ * @since 1.8
+ *        create 2021年12月27日
+ */
+public class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals == null || intervals[0].length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, (v1, v2) -> v1[1] - v2[1]);
+        int end = intervals[0][1];
+        int count = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (end <= intervals[i][0]) {
+                end = intervals[i][1];
+                count++;
+            }
+        }
+        return intervals.length - count;
+    }
+
+    public static void main(String[] args) {
+        int[][] test = new int[][] { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 1, 3 } };
+        Solution s = new Solution();
+        int result = s.eraseOverlapIntervals(test);
+        System.out.println(result);
+    }
+}
