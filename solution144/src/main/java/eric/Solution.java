9441a766010994532070d6ed5dcd5ce70c326657
Eric <luckwuwenjun@outlook.com>
2022-02-08 18:09:09 +0800

    修改遍历方法
    修改遍历方法

--- solution144/src/main/java/eric/Solution.java
+++ solution144/src/main/java/eric/Solution.java
@@ -1,10 +1,9 @@
 package eric;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution {
+
     public List<Integer> preorderTraversal(TreeNode root) {
         //非递归方法
         Stack<TreeNode> st = new Stack<>();
@@ -26,4 +25,42 @@ public class Solution {
         }
         return visited;
     }
+
+    /**
+     * 递归算法
+     * @param root
+     * @param result
+     */
+    private void recuriteTravel(TreeNode root,List<Integer> result){
+        if(root != null){
+            result.add(root.val);
+        }
+        if(root.left != null){
+            recuriteTravel(root.left,result);
+        }
+        if(root.right != null){
+            recuriteTravel(root.right,result);
+        }
+    }
+
+    /**
+     * 迭代算法
+     * @param root
+     * @param result
+     */
+    private void iterateTravle(TreeNode root,List<Integer> result){
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode r = root;
+        while (r != null || !stack.isEmpty()){
+
+            while (r != null){
+                result.add(r.val);
+                stack.push(r);
+                r = r.left;
+            }
+            r = stack.pop();
+            r = r.right;
+        }
+    }
+
 }
--- solution94/src/main/java/eric/Solution.java
+++ solution94/src/main/java/eric/Solution.java
@@ -1,7 +1,5 @@
 package eric;
 
-import com.sun.source.tree.Tree;
-
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
