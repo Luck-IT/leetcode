b72901c338595b7f8e83ba7c5fb6ce27cf067f60
Eric <luckwuwenjun@outlook.com>
2022-02-22 14:35:41 +0800

    完成53
    完成53

---
+++ solution53/src/main/java/eric/Solution.java
@@ -0,0 +1,15 @@
+package eric;
+
+/**
+ * Hello world!
+ *
+ */
+public class Solution{
+
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0 || nums.length == 1){
+            return nums == null ? 0:nums[0];
+        }
+        int result = Integer
+    }
+}
