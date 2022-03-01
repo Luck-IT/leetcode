00d64d6ecc59cf3ebf3a05d2d87af5f666e966d3
Eric <luckwuwenjun@outlook.com>
2022-03-01 16:07:32 +0800

    完成1005

--- solution1005/src/main/java/eric/Solution.java
+++ solution1005/src/main/java/eric/Solution.java
@@ -1,12 +1,38 @@
 package eric;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 /**
  * Hello world!
  *
  */
-public class Solution 
-{
+public class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
-
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        if (k == 0) {
+            for (int i = 0; i < nums.length; i++) {
+                result += nums[i];
+            }
+        } else {
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+            for(int i=0;i<nums.length;i++){
+                queue.add(nums[i]);
+            }
+            while(k>0){
+                Integer tmp = queue.poll();
+                tmp = tmp*(-1);
+                queue.add(tmp);
+                k--;
+            }
+            while(!queue.isEmpty()){
+                result+=queue.poll();
+            }
+        }
+        return result;
     }
 }
