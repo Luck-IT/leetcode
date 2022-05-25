0c149fa86709f19f7c371b42bba0159cf57c1c25
Eric Wu <wuwj107@chinaunicom.cn>
2022-05-25 02:48:27 +0000

    修改53 增加动态规划算法

--- solution53/src/main/java/eric/Solution.java
+++ solution53/src/main/java/eric/Solution.java
@@ -24,4 +24,22 @@ public class Solution {
         }
         return result;
     }
+
+    //递归算法
+    public int maxSubArray2(int[] nums){
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        //1 dp[i] 表示包括i之前的最大子序和
+        int[] dp = new int[nums.length];
+        //2 递推公式
+        // dp[i] = max(dp[i-1]+nums[i],nums[i])
+        //3 初始化 dp[0] = nums[0]
+        dp[0] = nums[0];
+        //4 确定递推顺序
+        for (int i=1;i<nums.length;i++){
+            dp[i] =Math.max(dp[i-1]+nums[i],nums[i]);
+        }
+        return dp[nums.length-1];
+    }
 }
